fe55292bfa1c2752d1467072022d0784789a422b
Jonas Bernoulli <jonas@bernoul.li>
2017-08-05 11:49:00 +0200

    [patch] Set file-local value of outline-regexp.
    The headings in this file don't follow the conventions, but
    by adjusting `outline-regexp' we can get it to work anyway.

--- undo-tree.el
+++ undo-tree.el
@@ -4764,4 +4764,7 @@ specifies `saved', and a negative prefix argument specifies
 
 (provide 'undo-tree)
 
+;; Local Variables:
+;; outline-regexp: ";;;\\(;* [^=\t\n]\\|###autoload\\)\\|("
+;; End:
 ;;; undo-tree.el ends here
